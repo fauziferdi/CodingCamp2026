036ec7b3b7c0db13421ba4130cce050db86e8b4e
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2026-04-21 09:39:48 +0700

    feat : oprator

---
+++ Belajar Dasar Pemrograman JavaScript/Mengarungi lauran Sintaksis/operator/main.js
@@ -0,0 +1,58 @@
+// Assignment Operator
+
+// Digunakan untuk menginisiasi nilai
+const name = 'Dicoding';
+let location = 'Bandung'
+
+// Digunakan untuk mengubah nilai
+location = 'Jakarta';
+
+
+// Arithmetic Operator
+
+let a = 10;
+let b = 5;
+
+console.log(a + b); // Penjumlahan
+console.log(a - b); // Pengurangan
+console.log(a * b); // Perkalian
+console.log(a / b); // Pembagian
+console.log(a % b); // Modulus
+
+// Comparison Operator
+
+let x = 10;
+let y = 12;
+
+console.log(x == y); // Sama dengan
+console.log(x != y); // Tidak sama dengan
+console.log(x > y); // Lebih besar dari
+console.log(x < y); // Lebih kecil dari
+console.log(x >= y); // Lebih besar dari atau sama dengan
+console.log(x <= y); // Lebih kecil dari atau sama dengan
+
+// Logical Operator
+// AND
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+console.log((5 === 5) && (3 < 5)); // true
+
+// OR
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log((5 === 5) || (3 > 5)); // true
+
+// NOT
+console.log(!true); // false
+console.log(!false); // true
+
+// String Operator
+
+const firstName = 'John';
+const lastName = 'Doe';
+
+console.log(firstName + ' ' + lastName); // John Doe
